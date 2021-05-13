db1ff2c43b29c9f3beb4a527321b2b4ae09dbfa3
Ved Malandkar <malandkarved@gmail.com>
2021-05-12 20:34:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,40 @@ GROUND_WIDTH, GROUND_HEIGHT = 100, 45
 GROUND_IMAGE = pygame.image.load(os.path.join('assets', 'ground.png'))
 
 
+<<<<<<< HEAD
 def draw_window(luther, ground):
     WIN.fill(WHITE)
     for i in [0, 1, 2, 3, 4, 5, 6, 7]:
         WIN.blit(GROUND_IMAGE, (ground[i].x, ground[i].y))
+=======
+def draw_window(luther, ground, ground1, ground2, ground3, ground4, ground5):
+    WIN.fill(WHITE)
+    WIN.blit(GROUND_IMAGE, (ground.x, ground.y))
+    WIN.blit(GROUND_IMAGE, (ground1.x, ground1.y))
+    WIN.blit(GROUND_IMAGE, (ground2.x, ground2.y))
+    WIN.blit(GROUND_IMAGE, (ground3.x, ground3.y))
+    WIN.blit(GROUND_IMAGE, (ground4.x, ground4.y))
+    WIN.blit(GROUND_IMAGE, (ground4.x, ground4.y))
+>>>>>>> parent of ec9254e (updated stuff)
     WIN.blit(MT_LUTHER, (luther.x, luther.y))
     pygame.display.update()
 
 
 def main():
     luther = pygame.Rect(200, 255, MT_LUTHER_WIDTH, MT_LUTHER_HEIGHT)
+<<<<<<< HEAD
     ground = []
     for i in [0, 1, 2, 3, 4, 5, 6, 7]:
         ground.append(pygame.Rect((100 * i), 455, GROUND_HEIGHT, GROUND_WIDTH)
+=======
+    ground = pygame.Rect(0, 455, GROUND_HEIGHT, GROUND_WIDTH)
+    ground1 = pygame.Rect(100, 455, GROUND_HEIGHT, GROUND_WIDTH)
+    ground2 = pygame.Rect(200, 455, GROUND_HEIGHT, GROUND_WIDTH)
+    ground3 = pygame.Rect(300, 455, GROUND_HEIGHT, GROUND_WIDTH)
+    ground4 = pygame.Rect(400, 455, GROUND_HEIGHT, GROUND_WIDTH)
+    ground5 = pygame.Rect(500, 455, GROUND_HEIGHT, GROUND_WIDTH)
+
+>>>>>>> parent of ec9254e (updated stuff)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -41,6 +62,7 @@ def main():
 
         keys_pressed=pygame.key.get_pressed()
         if keys_pressed[pygame.K_LEFT]:
+<<<<<<< HEAD
             for i in [0, 1, 2, 3, 4, 5, 6, 7]:
                 ground[i].x += VEL
 
@@ -49,6 +71,24 @@ def main():
                 ground[i].x -= VEL
 
         draw_window(luther, ground)
+=======
+            ground.x += VEL
+            ground1.x += VEL
+            ground2.x += VEL
+            ground3.x += VEL
+            ground4.x += VEL
+            ground5.x += VEL
+
+        if keys_pressed[pygame.K_RIGHT]:
+            ground.x -= VEL
+            ground1.x -= VEL
+            ground2.x -= VEL
+            ground3.x -= VEL
+            ground4.x -= VEL
+            ground5.x -= VEL
+        
+        draw_window(luther, ground, ground1, ground2, ground3, ground4, ground5)
+>>>>>>> parent of ec9254e (updated stuff)
 
     pygame.quit()
 
